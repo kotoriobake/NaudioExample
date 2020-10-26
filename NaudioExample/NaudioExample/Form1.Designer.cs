c378d457bb5e4f8dda111e69c941d300bfc951e6
DESKTOP-L2UCH8C\kotor <kotoriobake1@gmail.com>
2020-10-26 16:58:22 +0900

    OxyPlotによる計測データ表示・FFT結果表示機能を実装

---
+++ NaudioExample/NaudioExample/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace NaudioExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbDevice = new System.Windows.Forms.ComboBox();
+            this.cbAutoAxis = new System.Windows.Forms.CheckBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.oxyPlot1 = new OxyPlot.WindowsForms.PlotView();
+            this.oxyPlot2 = new OxyPlot.WindowsForms.PlotView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "InputAudioDevice";
+            // 
+            // cbDevice
+            // 
+            this.cbDevice.FormattingEnabled = true;
+            this.cbDevice.Location = new System.Drawing.Point(12, 24);
+            this.cbDevice.Name = "cbDevice";
+            this.cbDevice.Size = new System.Drawing.Size(121, 20);
+            this.cbDevice.TabIndex = 2;
+            // 
+            // cbAutoAxis
+            // 
+            this.cbAutoAxis.AutoSize = true;
+            this.cbAutoAxis.Location = new System.Drawing.Point(168, 26);
+            this.cbAutoAxis.Name = "cbAutoAxis";
+            this.cbAutoAxis.Size = new System.Drawing.Size(71, 16);
+            this.cbAutoAxis.TabIndex = 3;
+            this.cbAutoAxis.Text = "AutoAxis";
+            this.cbAutoAxis.UseVisualStyleBackColor = true;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(245, 9);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(70, 43);
+            this.btStart.TabIndex = 4;
+            this.btStart.Text = "start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(333, 9);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(68, 43);
+            this.btStop.TabIndex = 5;
+            this.btStop.Text = "stop";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // oxyPlot1
+            // 
+            this.oxyPlot1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.oxyPlot1.Location = new System.Drawing.Point(14, 68);
+            this.oxyPlot1.Name = "oxyPlot1";
+            this.oxyPlot1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.oxyPlot1.Size = new System.Drawing.Size(752, 274);
+            this.oxyPlot1.TabIndex = 6;
+            this.oxyPlot1.Text = "plotView1";
+            this.oxyPlot1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.oxyPlot1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.oxyPlot1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // oxyPlot2
+            // 
+            this.oxyPlot2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.oxyPlot2.Location = new System.Drawing.Point(14, 348);
+            this.oxyPlot2.Name = "oxyPlot2";
+            this.oxyPlot2.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.oxyPlot2.Size = new System.Drawing.Size(752, 278);
+            this.oxyPlot2.TabIndex = 7;
+            this.oxyPlot2.Text = "plotView1";
+            this.oxyPlot2.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.oxyPlot2.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.oxyPlot2.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 638);
+            this.Controls.Add(this.oxyPlot2);
+            this.Controls.Add(this.oxyPlot1);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.cbAutoAxis);
+            this.Controls.Add(this.cbDevice);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbDevice;
+        private System.Windows.Forms.CheckBox cbAutoAxis;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.Timer timer1;
+        private OxyPlot.WindowsForms.PlotView oxyPlot1;
+        private OxyPlot.WindowsForms.PlotView oxyPlot2;
+    }
+}
+
---
+++ NaudioExample/NaudioExample/Form1.cs
@@ -0,0 +1,197 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NaudioExample
+{
+    public partial class Form1 : Form
+    {
+        int gSampleRate = 44100;
+        int gBitRate = 16;
+        int gChannels = 1;
+        int gBufferMilliseconds = 20;
+
+        bool gPlotDataX_flag = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ScanSoundCards();
+            PlotInitialize();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void ScanSoundCards()
+        {
+            cbDevice.Items.Clear();
+            for (int i = 0; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+                cbDevice.Items.Add(NAudio.Wave.WaveIn.GetCapabilities(i).ProductName);
+            if (cbDevice.Items.Count > 0)
+                cbDevice.SelectedIndex = 0;
+            else
+                MessageBox.Show("ERROR: no recording devices available");
+        }
+
+        double[] dataX;
+        private void PlotInitialize()
+        {
+            if (dataFft != null)
+            {
+                double fftSpacing = (double)wvin.WaveFormat.SampleRate / dataFft.Length;
+                dataX = new double[dataFft.Length];
+
+                for(int i=0; i<dataFft.Length;i++)
+                {
+                    dataX[i] = fftSpacing * i;
+                }
+                gPlotDataX_flag = true;
+            }
+        }
+
+        private NAudio.Wave.WaveInEvent wvin;
+        private void AudioMonitorInitialize(
+                int DeviceIndex, int sampleRate = 44100,
+                int bitRate = 16, int channels = 1,
+                int bufferMilliseconds = 100, bool start = true
+            )
+        {
+            if (wvin == null)
+            {
+                wvin = new NAudio.Wave.WaveInEvent();
+                wvin.DeviceNumber = DeviceIndex;
+                wvin.WaveFormat = new NAudio.Wave.WaveFormat(sampleRate, bitRate, channels);
+                wvin.DataAvailable += OnDataAvailable;
+                wvin.BufferMilliseconds = bufferMilliseconds;
+                if (start)
+                    wvin.StartRecording();
+            }
+        }
+
+        Int16[] dataPcm;
+        private int buffersRead = 0;
+        private void OnDataAvailable(object sender, NAudio.Wave.WaveInEventArgs args)
+        {
+            int bytesPerSample = wvin.WaveFormat.BitsPerSample / 8;
+            int samplesRecorded = args.BytesRecorded / bytesPerSample;
+            if (dataPcm == null)
+                dataPcm = new Int16[samplesRecorded];
+            for (int i = 0; i < samplesRecorded; i++)
+                dataPcm[i] = BitConverter.ToInt16(args.Buffer, i * bytesPerSample);
+            /*
+            int sampleRate = 44100;
+            short[] buffer = new short[2205];
+            double amplitude = 0.25 * short.MaxValue;
+            double frequency = 1000;
+            for (int n = 0; n < buffer.Length; n++)
+            {
+                dataPcm[n] = (short)(amplitude * Math.Sin((2 * Math.PI * n * frequency) / sampleRate));
+            }
+            */
+        }
+
+        double[] dataFft;
+
+        private void updateFFT()
+        {
+            // the PCM size to be analyzed with FFT must be a power of 2
+            int fftPoints = 2;
+            while (fftPoints * 2 <= dataPcm.Length)
+                fftPoints *= 2;
+
+            // apply a Hamming window function as we load the FFT array then calculate the FFT
+            NAudio.Dsp.Complex[] fftFull = new NAudio.Dsp.Complex[fftPoints];
+            for (int i = 0; i < fftPoints; i++)
+                fftFull[i].X = (float)(dataPcm[i] * NAudio.Dsp.FastFourierTransform.HammingWindow(i, fftPoints));
+            NAudio.Dsp.FastFourierTransform.FFT(true, (int)Math.Log(fftPoints, 2.0), fftFull);
+
+            // copy the complex values into the double array that will be plotted
+            if (dataFft == null)
+                dataFft = new double[fftPoints];
+            for (int i = 0; i < fftPoints; i++)
+            {
+                double fftLeft = Math.Abs(fftFull[i].X + fftFull[i].Y);
+                double fftRight = Math.Abs(fftFull[fftPoints - i - 1].X + fftFull[fftPoints - i - 1].Y);
+                dataFft[i] = fftLeft + fftRight;
+                //dataFft[i] = Math.Sqrt(fftFull[i].X * fftFull[i].X + fftFull[i].Y * fftFull[i].Y);
+            }
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            AudioMonitorInitialize(cbDevice.SelectedIndex, gSampleRate, gBitRate, gChannels, gBufferMilliseconds, true);
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            if (wvin != null)
+            {
+                wvin.StopRecording();
+                wvin = null;
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (dataPcm == null)
+                return;
+
+            if (!gPlotDataX_flag)
+            {
+                PlotInitialize();
+            }
+
+            updateFFT();
+
+            if (cbAutoAxis.Checked)
+            {
+
+            }
+            if (dataX != null)
+            {
+                PlotModel model = new PlotModel();
+                PlotModel t_model = new PlotModel();
+
+                var line = new OxyPlot.Series.LineSeries()
+                {
+                    Title = $"Series 1",
+                    Color = OxyPlot.OxyColors.Blue,
+                    StrokeThickness = 1,
+                    MarkerSize = 2,
+                    MarkerType = OxyPlot.MarkerType.Circle
+                };
+
+                var line2 = new OxyPlot.Series.LineSeries()
+                {
+                    Title = $"Series t",
+                    Color = OxyPlot.OxyColors.Blue,
+                    StrokeThickness = 1,
+                    MarkerSize = 2,
+                    MarkerType = OxyPlot.MarkerType.Circle
+                };
+
+                for (int i = 0; i < dataX.Length; i++)
+                    line.Points.Add(new OxyPlot.DataPoint(dataX[i], dataFft[i]));
+
+                for (int i = 0; i < dataPcm.Length; i++)
+                    line2.Points.Add(new OxyPlot.DataPoint(i, dataPcm[i]));
+
+                model.Series.Add(line);
+                oxyPlot1.Model = model;
+
+                t_model.Series.Add(line2);
+                oxyPlot2.Model = t_model;
+            }
+        }
+    }
+}
